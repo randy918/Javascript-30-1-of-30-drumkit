9c35f5aa776cf90690a3da04f8adaa784ce6d43b
randy918 <randy918@gmail.com>
2022-01-16 15:25:17 -0600

    concurrent restart of sound effects for rapid use

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ window.addEventListener("keydown", function (e) {
   const audio = document.querySelector(`audio[data-key="${e.key}"]`);
 
   if (!audio) return;
+  audio.currentTime = 0;
   audio.play();
 
   console.log(audio);
