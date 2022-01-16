70e08a55bb0d27532b2a7a77ab7b73a509b04257
randy918 <randy918@gmail.com>
2022-01-16 16:19:14 -0600

    keyvoard moves plays and erases motion

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 window.addEventListener("keydown", function (e) {
   const audio = document.querySelector(`audio[data-key="${e.key}"]`);
-
+  const key = document.querySelector(`.key[data-key="${e.key}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
-
-  console.log(audio);
+  key.classList.add("playing");
+  console.log(key);
 });
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+console.log(keys);
